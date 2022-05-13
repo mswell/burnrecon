e6c1dd5b8d625f7d5b642d08f679bcb71cb76fbb
Wellington Moraes <wellpunk@gmail.com>
2022-05-13 16:09:54 -0300

    add list-subs options

--- burnrecon/cli.py
+++ burnrecon/cli.py
@@ -7,30 +7,22 @@ Be creative! do whatever you want!
 - Start a web application
 - Import things from your .base module
 """
-import sys
-from base import subdomain_enum
+import typer
+from base import subdomain_enum, list_subdomains
+
+app = typer.Typer()
 
 
-target = sys.argv[1]
-domain = sys.argv[2]
-subdomain_enum(target, domain)
+@app.command()
+def enum(target: str, domain: str):
+    subdomain_enum(target, domain)
 
 
-def main():
+@app.command()
+def list_subs(target: str):
+    for subs in list_subdomains(target):
+        print(subs["subdomain"])
 
-    # pragma: no cover
-    """
-    The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
 
-    This is your program's entry point.
-
-    You can change this function to do whatever you want.
-    Examples:
-        * Run a test suite
-        * Run a server
-        * Do some other stuff
-        * Run a command line application (Click, Typer, ArgParse)
-        * List all available tasks
-        * Run an application (Flask, FastAPI, Django, etc.)
-    """
+if __name__ == "__main__":
+    app()
