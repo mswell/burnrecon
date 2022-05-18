54e3dad80aece7efa34fcf604d27146af0b409f9
Wellington Moraes <wellpunk@gmail.com>
2022-05-18 17:02:08 -0300

    create the validation of the domain argument

--- burnrecon/base.py
+++ burnrecon/base.py
@@ -1,10 +1,19 @@
+import sys
+
+import validators
 from database import connect_db
 from getalive import httpx_parser
 from subdomain_parse import run_sub_parser
 
 
 def subdomain_enum(target, domain):
-    run_sub_parser(target, domain)
+    valid_domain = validators.domain(domain)
+    if valid_domain:
+        run_sub_parser(target, domain)
+    else:
+        raise NameError(
+            sys.exit(f"{domain} is not a valid domain patterns, ex: example.com")
+        )
 
 
 def list_subdomains(target):
