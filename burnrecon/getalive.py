2aa21dca6168be9df21d0b066c4f75d9f9d74e1d
João Costa <joaocosta_neto@hotmail.com>
2022-05-20 12:42:57 -0300

    1.1
    deixando de usar o multithreads do python pra usar o do proprio httpx, assim vai abrir apenas 1 processo do httpx no pc em vez de varios

--- burnrecon/getalive.py
+++ burnrecon/getalive.py
@@ -1,56 +1,55 @@
-import concurrent.futures
-import json
-import os
 import tempfile
-from datetime import datetime
-from pathlib import Path
-
+import json
+import concurrent.futures
 from database import connect_db
+from pathlib import Path
+from datetime import datetime
+import subprocess
 
 random_name = tempfile.NamedTemporaryFile(delete=False)
-final_file = Path(random_name.name)
-
+subdomains_file = Path(random_name.name)
 httpx_tech = {}
+httpx_response = ""
 
 
-def setup_httpx(subdomain):
-    random_name = tempfile.NamedTemporaryFile(delete=False)
-    httpx_out = Path(random_name.name)
+def setup_httpx():
+    global httpx_response
     httpx_cmd = f"httpx -silent -status-code -tech-detect -timeout 10 "
-    httpx_cmd += f"-threads 10 -json -o {httpx_out}"
-    os.system(f"echo {subdomain } | {httpx_cmd}")
-    os.system(f"cat {httpx_out} >> {final_file}")
-    httpx_out.unlink()
+    httpx_cmd += f"-threads 10 -json -silent"
+    httpx_response = subprocess.check_output(
+        f"cat {subdomains_file} | {httpx_cmd}", shell=True).decode("utf-8") 
+    subdomains_file.unlink()
 
 
 def run_httpx(target):
     db = connect_db()
     subs_collection = db["subdomains"]
     query = subs_collection.find({"target": target})
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = []
-        for result in query:
-            futures.append(executor.submit(setup_httpx, result["subdomain"]))
+    subdomains = [result["subdomain"] for result in query]
+    with open(subdomains_file, "w") as file_subdomains:
+        file_subdomains.write("\n".join(subdomains))
 
+    setup_httpx()
 
 def httpx_parser(target):
     run_httpx(target)
     db = connect_db()
     collection = db["alivehosts"]
-    with open(final_file, "r") as f:
-        for line in f:
-            json_data = json.loads(line)
-            if "technologies" in json_data:
-                httpx_tech["tech"] = json_data["technologies"]
-            else:
-                httpx_tech["tech"] = ""
+    data_list = []
+    for line in httpx_response.split("\n"):
+        if(line == ""): continue
+        json_data = json.loads(line)
+        if "technologies" in json_data:
+            httpx_tech["tech"] = json_data["technologies"]
+        else:
+            httpx_tech["tech"] = ""
 
-            if "webserver" in json_data:
-                httpx_tech["webserver"] = json_data["webserver"]
-            else:
-                httpx_tech["webserver"] = ""
+        if "webserver" in json_data:
+            httpx_tech["webserver"] = json_data["webserver"]
+        else:
+            httpx_tech["webserver"] = ""
 
-            data = {
+        data = {
                 "target": target,
                 "url": json_data["url"],
                 "status-code": json_data["status-code"],
@@ -60,9 +59,10 @@ def httpx_parser(target):
                 "techs": httpx_tech["tech"],
                 "date": datetime.now(),
             }
-            if collection.find_one({"url": json_data["url"]}):
-                print(f"{json_data['url']} already exists id DB")
-            else:
-                collection.insert_one(data)
-                print(f"Inserted {json_data['url']}")
-        final_file.unlink()
+        if collection.find_one({"url": json_data["url"]}):
+            print(f"{json_data['url']} already exists id DB")
+        else:
+            data_list.append(data)
+            print(f"Inserted {json_data['url']}")
+    if (len(data_list) > 0):
+        collection.insert_many(data_list)
